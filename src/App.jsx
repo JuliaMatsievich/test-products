c9267f85e0d5110aa4b7613ef66d6d3a8c27c6d8
JuliaMatsievich <juliamatsievich@mail.ru>
2024-02-26 15:24:25 +0500

    add: api

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,27 @@
-import './App.css'
-import { Header } from './components/Header/Header'
+import md5 from 'js-md5';
+import './App.css';
+import { Header } from './components/Header/Header';
+import { useEffect } from 'react';
 
 function App() {
-  // Функция запроса GET авторизованного пользователя
-  async function fetchGetProducts() {
-    return fetch(`http://api.valantis.store:40000/`, {
-      method: 'POST',
-      headers: {
-        'X-Auth': `md5("Valantis_20240224")`,
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-      body: JSON.stringify({
-        action: 'get_ids',
-        params: { offset: 10, limit: 3 },
-      }),
-    })
-    .then((response) => console.log(response))
-      // .then((response) => response.json())
-      // .then((data) => console.log(data))
-      .catch((error) => console.error('Ошибка:', error))
-  }
 
-  fetchGetProducts()
+  useEffect(() => {
+        fetch(`http://api.valantis.store:40000/`, {
+          method: 'POST',
+          headers: {
+            'X-Auth': md5("Valantis_20240226"),
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            action: 'get_ids',
+            params: { offset: 10, limit: 3 },
+          }),
+        })
+          .then((response) => response.json())
+          .then((data) => console.log(data))
+          .catch((error) => console.error('Ошибка:', error))
+  },[])
+
 
   return (
     <>
